7d63bac79c1076fef61b60a408a671a954bf68a4
ITSC <ITSC@DESKTOP-T0J7H4M>
2023-12-06 15:49:13 +0900

    ActionDelegate 추가

--- com.js.favorites/src/com/js/favorites/actions/AddToFavoritesActionDelegate.java
+++ com.js.favorites/src/com/js/favorites/actions/AddToFavoritesActionDelegate.java
@@ -1,5 +1,85 @@
 package com.js.favorites.actions;
 
-public class AddToFavoritesActionDelegate {
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+public class AddToFavoritesActionDelegate implements IObjectActionDelegate {
+	
+	private IWorkbenchPart targetPart;
+	private IProject project;
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method 
+		//만약에 한다고하면 여기서 소스 파싱 관련된 기능이 필요함.
+		MessageDialog.openInformation(this.targetPart.getSite().getShell(),"add to Favorites " , "triggered the " + getClass().getName() + " action");
+		
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		project = root.getProject("teste1111");
+		
+		
+		System.out.println("######## " + project);
+		if (!project.isOpen())
+			try {
+				project.open(null);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		if (!project.exists())
+			try {
+				project.create(null);
+				
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		
+		System.out.println("######## " + project);
+		
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	    if (window != null)
+	    {
+	        IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+	        Object firstElement = selection.getFirstElement();
+	        if (firstElement instanceof IAdaptable)
+	        {
+	            IProject project = (IProject)((IAdaptable)firstElement).getAdapter(IProject.class);
+	            IPath path = project.getFullPath();
+	            System.out.println(path);
+	        }
+	    }
+	    
+	    
+		
+	
+		
+		
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// TODO Auto-generated method stub
+		this.targetPart = targetPart;
+		
+	}
+
 
 }
